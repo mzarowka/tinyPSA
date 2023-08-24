cc4d1bd7de356e329f36e4f21f434dd6647c6894
mzarowka <maurycy.zarczynski@outlook.com>
2023-08-24 15:43:44 -0700

    Prepare gstat_populate()

--- R/tpsa_gstat.R
+++ R/tpsa_gstat.R
@@ -62,5 +62,55 @@ tpsa_gstat_read <- function(path){
 #' @return  invisible tibble with fitting GRADISTAT data structure.
 #' @export
 tpsa_gstat_populate <- function(path, gradistat){
+  # Get data file name
+  file_data <- basename(path) |>
+    # Remove extension
+    fs::path_ext_remove()
 
+  # Get GRADISTAT file name
+  file_gradistat <- basename(path) |>
+    # Remove extension
+    fs::path_ext_remove()
+
+  cli::cli_alert_info("Reading data and GRADISTAT files: {file_data} and {file_gradistat}.")
+
+  # Read data
+  data <- path |>
+    # Read data anchored at cell C4
+    readxl::read_excel(sheet = "Multiple Sample Statistics",
+                       range = cellranger::cell_limits(c(4, 3), c(NA, NA))) |>
+    # Bind with variable names
+    dplyr::bind_cols(gstat_vars[1]) |>
+    # Relocate variable names to front
+    dplyr::relocate(variable)
+
+  gstat_descriptors <- data |>
+    # Keep only description rows
+    dplyr::filter(variable %in% gstat_desc) |>
+    # Pivot longer
+    tidyr::pivot_longer(-variable, names_to = "sample", values_to = "value") |>
+    # Pivot wider
+    tidyr::pivot_wider(names_from = variable, values_from = value)
+
+  gstat_num <- data |>
+    # Keep only numeric variables
+    dplyr::filter(!variable %in% gstat_desc) |>
+    # Convert to numeric
+    dplyr::mutate(dplyr::across(-variable, \(x) as.numeric(x))) |>
+    # Pivot longer
+    tidyr::pivot_longer(-variable, names_to = "sample", values_to = "value") |>
+    # Pivot wider
+    tidyr::pivot_wider(names_from = variable, values_from = value)
+
+  data <- dplyr::left_join(gstat_descriptors, gstat_num, dplyr::join_by(sample == sample))
+
+  cli::cli_alert_info("Writing cleaned {file_name}.xlsx.")
+
+  # Write to file
+  writexl::write_xlsx(data, path = fs::path(fs::path_dir(path), file_name, ext = "xlsx"))
+
+  cli::cli_alert_success("Finished {file_name}.")
+
+  # Return data
+  data
 }
